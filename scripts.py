80b74a38b9377849b7f07dbd51ebbd0bead138f5
hamza <elfarre.hamza@gmail.com>
2020-06-15 21:25:48 +0100

    removed pip files1

--- scripts.py
+++ scripts.py
@@ -10,7 +10,7 @@ import pandas_datareader as web
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
-# import tensorflow as tf
+import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LSTM,Dropout
 from json import JSONEncoder
